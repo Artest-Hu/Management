955e03636764ff5225ed5e13628281cd5eb441f1
Artest_Hu <101932637+Artest-Hu@users.noreply.github.com>
2026-01-08 13:33:08 -0500

    Create User.java

---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,69 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "age", nullable = false)
+    private Integer age;
+    @Column(name = "salary", nullable = false)
+    private Double salary;
+
+
+    public User() {
+
+    }
+    public User(Long id, String name, Integer age, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+}
